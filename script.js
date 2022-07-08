55f3eb0e17aacd0f3ded4728ec59baf5c5799217
Tanya Buechel <tanyabuechel@gmail.com>
2022-07-07 21:52:21 -0500

    Added validateInput, formSubmission, addDestinationInfo, and pickPlanet functions

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+   
+    //Add alert to notify the user that all fields are required
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        const pilotName = document.querySelector('input[name=pilotName]');
+        const copilotName = document.querySelector('input[name=copilotName]');
+        const fuelLevel = document.querySelector('input[name=fuelLevel');
+        const cargoMass = document.querySelector('input[name=cargoMass');
+             if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            alert("All fields are required.");
+            event.preventDefault();
+            }
+        
+    })
+
+
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,48 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput.value === "") {
+    return("Empty");
+   }
+   if (isNaN(testInput.value) === true) {
+    return("Not a Number");
+   }
+   if (isNaN(testInput.value) === false) {
+    return("Is a Number");
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+//Use values in strings to update the shuttle requirements
+
+   //Pilot and Co-pilot Update
+    document.getElementById('pilotStatus').innerHTML = `Pilot ${pilot} is ready for launch`;
+    document.getElementById('copilotStatus').innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
+   //Fuel Level Update
+    if (fuelLevel < 10000) {
+        document.getElementByClassName('faultyItems').styles.visibility = "visible";
+        document.getElementById('fuelStatus').innerHTML = `Fuel level too low for launch`;
+        document.getElementById('launchStatus').innerHTML = `Shuttle Not Ready for Launch`;
+        document.getElementById('launchStatus').styles.color = 'red';
+    }
+
+   //Cargo Mass Update
+    if (cargoLevel > 10000) {
+        document.getElementByClassName('faultyItems').style.visibility = "visible";
+        document.getElementById('cargoStatus').innerHTML = `Cargo mass is too high for launch`;
+        document.getElementById('launchStatus').innerHTML = `Shuttle Not Ready for Launch`;
+        document.getElementById('launchStatus').styles.color = 'red';
+    }
+
+   //Ready for Launch
+    if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+        document.getElementById('launchStatus').styles.color = 'green';
+        document.getElementById('launchStatus').innerHTML = `Shuttle is Ready for Launch!`;
+    }
+
+    //Going to use the validateInput() to complete this function
+
 }
 
 async function myFetch() {
